156ef09a6d3d326de7e5630db97a0fc4d70ff87e
Przemyslaw Strucinski <pst@kmdpoland.pl>
2019-02-20 16:37:35 +0100

    Changes for exercises

--- src/exercises/classes_interfaces/classes_intefaces_deeper2.spec.ts
+++ src/exercises/classes_interfaces/classes_intefaces_deeper2.spec.ts
@@ -1,71 +1,28 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 
 
 
-describe('Interfaces and classes - deeper', () => {
+// describe('Interfaces and classes - deeper', () => {
 
-    it('Exercise 2', () => {
-        let list = new MyList();
-        expect(list.length).to.equal(0);
+//     it('Exercise 2', () => {
+//         let list = new MyList();
+//         expect(list.length).to.equal(0);
 
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
+//         list.add("Element1");
+//         list.add("Element2");
+//         list.add("Element3");
 
-        expect(list.length).to.equal(3);
-        expect(list.first()).to.equal("Element1");
-        expect(list.first()).to.equal(list.getAll()[0]);
+//         expect(list.length).to.equal(3);
+//         expect(list.first()).to.equal("Element1");
+//         expect(list.first()).to.equal(list.getAll()[0]);
 
-        list.remove("Element2");
-        expect(list.length).to.equal(2);
+//         list.remove("Element2");
+//         expect(list.length).to.equal(2);
 
-        let listNextInstance = new MyList(list);
-        expect(listNextInstance.length).to.equal(2);
+//         let listNextInstance = new MyList(list);
+//         expect(listNextInstance.length).to.equal(2);
 
-        expect(list.add('NewElement').getAll().length).to.equal(3);
+//         expect(list.add('NewElement').getAll().length).to.equal(3);
 
-    });
-
-
-    interface IMyList{
-        add (value: string): IMyList;
-        remove (value: string): IMyList,
-        first() : string,
-        getAll(): string[]
-    }
-
-    class MyList implements IMyList{
-
-        private list: string[];
-
-        get length(): number {
-            return this.list.length;
-        }
-
-        constructor(myList: MyList = null) {
-            if (myList) 
-                this.list = myList.list 
-            else 
-                this.list = [];
-        }
-
-        add (value: string): MyList {
-            this.list.push(value);
-            return this;
-        }
-
-        remove (value: string): MyList {
-            const index = this.list.findIndex(item => item === value);
-            this.list.splice(index, 1);
-            return this;
-        }
-
-        first() : string {
-            return this.list[0];
-        }
-
-        getAll(): string[] {
-            return this.list;
-        }
-    }
-})
+//     });
+// })
--- src/exercises/generics_enums/generics_enums_fundamental.spec.ts
+++ src/exercises/generics_enums/generics_enums_fundamental.spec.ts
@@ -1,87 +1,29 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 
 
 
-describe('Generics and enums- fundamentals exercise', () => {
+// describe('Generics and enums- fundamentals exercise', () => {
 
-    it('Exercise 1', () => {
-        let list = new MyList<string>();
-        expect(list.length).to.equal(0);
+//     it('Exercise 1', () => {
+//         let list = new MyList<string>();
+//         expect(list.length).to.equal(0);
 
-        list.add("Element1");
-        list.add("Element2");
-        list.add("Element3");
+//         list.add("Element1");
+//         list.add("Element2");
+//         list.add("Element3");
 
-        expect(list.length).to.equal(3);
-        expect(list.first()).to.equal("Element1");
-        expect(list.first()).to.equal(list.getAll()[0]);
+//         expect(list.length).to.equal(3);
+//         expect(list.first()).to.equal("Element1");
+//         expect(list.first()).to.equal(list.getAll()[0]);
 
-        list.remove("Element2");
-        expect(list.length).to.equal(2);
+//         list.remove("Element2");
+//         expect(list.length).to.equal(2);
 
-        let listNextInstance = new MyList<string>(list);
-        expect(listNextInstance.length).to.equal(2);
+//         let listNextInstance = new MyList<string>(list);
+//         expect(listNextInstance.length).to.equal(2);
 
-        expect(list.add('NewElement').getAll().length).to.equal(3);
+//         expect(list.add('NewElement').getAll().length).to.equal(3);
 
-        expect(list.operation(Direction.Add, "ElementAddedWithEnum").length).to.equal(4);
+//         expect(list.operation(Operation.Add, "ElementAddedWithEnum").length).to.equal(4);
 
-    });
-
-    enum Direction {
-        Add,
-        Remove
-    }
-
-    interface IMyList<T>{
-        add (value: T): MyList<T>,
-        remove (value: T): MyList<T>,
-        first() : T,
-        getAll(): T[]
-    }
-
-    class MyList<T> implements IMyList<T> {
-
-        private list: T[];
-
-        get length(): number {
-            return this.list.length;
-        }
-
-        constructor(myList: MyList<T> = null) {
-            if (myList) 
-                this.list = myList.list 
-            else 
-                this.list = [];
-        }
-
-        add (value: T): MyList<T> {
-            this.list.push(value);
-            return this;
-        }
-
-        remove (value: T): MyList<T> {
-            const index = this.list.findIndex(item => item === value);
-            this.list.splice(index, 1);
-            return this;
-        }
-
-        first() : T {
-            return this.list[0];
-        }
-
-        getAll(): T[] {
-            return this.list;
-        }
-
-        operation(type: Direction, value?: T): MyList<T>  {
-            if(type === Direction.Add) {
-                return this.add(value);
-            }
-            if(type === Direction.Remove) {
-                return this.remove(value);
-            }
-        }
-    }
-
-})
+//     });
