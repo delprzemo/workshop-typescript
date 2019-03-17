3c10e1a3e455ed891d697c4917ad586e051534ac
Przemyslaw Strucinski <pst@kmdpoland.pl>
2019-03-17 17:10:23 +0100

    Add new ambient module - resolve-options.spec.ts

--- src/09_modules/modules_3_ambient/object.pick.spec.ts
+++ src/09_modules/modules_3_ambient/object.pick.spec.ts
@@ -1,21 +1,21 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 
-// https://github.com/jonschlinkert/object.pick
-var pick = require('object.pick');
+// // https://github.com/jonschlinkert/object.pick
+// var pick = require('object.pick');
 
-describe('Object pick ambient module', () => {
+// describe('Object pick ambient module', () => {
     
-    // Advanced
-    it('Run with one key', () => {
-        let result = pick({a: 'a', b: 'b'}, 'a');
+//     // Advanced
+//     it('Run with one key', () => {
+//         let result = pick({a: 'a', b: 'b'}, 'a');
 
-        expect(result).to.deep.equal({a: 'a'});
-    });
+//         expect(result).to.deep.equal({a: 'a'});
+//     });
 
-    it('Run with multiple key', () => {
-        let result = pick({a: 'a', b: 'b', c: 2}, ['a', 'c']);
+//     it('Run with multiple key', () => {
+//         let result = pick({a: 'a', b: 'b', c: 2}, ['a', 'c']);
 
-        expect(result).to.deep.equal({a: 'a', c: 2});
-    });
+//         expect(result).to.deep.equal({a: 'a', c: 2});
+//     });
 
-});
+// });
---
+++ src/09_modules/modules_3_ambient/resolve-options.spec.ts
@@ -0,0 +1,51 @@
+// import { expect } from 'chai';
+
+// // https://github.com/gulpjs/resolve-options
+// var createResolver = require('resolve-options');
+
+
+// describe('Resolve options', () => {
+    
+//     // Advanced
+//     it('Run with one key', () => {
+//         var config = {
+//           cwd: {
+//             type: 'string',
+//             default: process.cwd
+//           },
+//           sourcemaps: {
+//             type: 'boolean',
+//             default: false
+//           },
+//           since: {
+//             type: ['date', 'number']
+//           },
+//           read: {
+//             type: 'boolean'
+//           }
+//         };
+        
+//         var options = {
+//           sourcemaps: true,
+//           since: Date.now(),
+//           read: function() {
+//             return true;
+//           }
+//         };
+        
+//         var resolver = createResolver(config, options);
+        
+//         var cwd = resolver.resolve('cwd');
+//         // cwd === process.cwd()
+        
+//         var sourcemaps = resolver.resolve('sourcemaps');
+//         // sourcemaps === true
+        
+//         var read = resolver.resolve('read');
+
+//         expect(cwd).to.equal(process.cwd());
+//         expect(sourcemaps).to.equal(true);
+//         expect(read).to.equal(true);
+//     });
+
+// });
