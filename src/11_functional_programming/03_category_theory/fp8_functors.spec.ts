c539b3f32284fc16fcb0a4e88639d8beafdba905
Przemyslaw Strucinski <pst@kmdpoland.pl>
2019-03-27 07:44:22 +0100

    FP : functors, applicative

---
+++ src/11_functional_programming/03_category_theory/fp8_functors.spec.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+
+describe('Functional programming - functors', () => {
+
+    // it('Example', () => {
+    //     class Container<T> {
+
+    //         private _value: T;
+        
+    //         public constructor(val: T) {
+    //             this._value = val;
+    //         }
+        
+    //         // Exercise - create map function
+
+    //         //only for assert
+    //         get value() {
+    //             return this._value;
+    //         }
+        
+    //     }
+    
+    //     const double = (x: number) => x + x;
+    //     const container = new Container(3);
+    //     const container2 = container.map(double);
+
+    //     expect(container2.value).to.equal(6);
+    // });
+});
---
+++ src/11_functional_programming/03_category_theory/fp9_applicative.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+
+describe('Functional programming - applicative', () => {
+
+    // it('Example', () => {
+    //     class Container<T> {
+    //         private _value!: T;
+        
+    //         public constructor(val: T) {
+    //             this._value = val;
+    //         }
+
+    //         // Exercise - create map, app and of functions 
+
+    //         //only for assert
+    //         get value() {
+    //             return this._value;
+    //         }
+    //     }
+    
+    //     let double = (x: number) => x + x;
+    //     let numberContainer = Container.of(3);
+    
+    //     // Ordinary functors
+    //     let newContainer = numberContainer.map(double);
+    
+    //     // Applicative
+    //     let functionContainer = Container.of(double);
+    //     let newContainer2 = numberContainer.ap(functionContainer);
+
+    //     expect(newContainer.value).to.equal(6);
+    //     expect(newContainer2.value.value).to.equal(6);
+    // });
+});
